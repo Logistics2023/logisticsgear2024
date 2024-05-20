d11e36ed366eeac52a61c2007ba99c5c545dacff
swoou <proyection009@gmail.com>
2024-05-20 11:30:09 -0600

    DC

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,16 @@ export default function BottomNavigation({ rol }) {
                 <Link href="/" className="flex items-center">
                     <img src="/logo.svg" className="h-[50px] mr-3" alt="Flowbite Logo" />
                 </Link>
+                <div className="relative w-full">
+                    <input type="search" id="location-search" className="block p-3 w-full h-full z-20 text-sm text-gray-900   rounded-[5px] focus:ring-blue-500 focus:border-blue-500" placeholder="Codigo de tracking" required />
+                    <button type="submit" className="absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-[5px] border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                      <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                      </svg>
+                      <span className="sr-only">Search</span>
+                    </button>
 
+                  </div>
 
 
                 {pathname !== '/Login' && pathname !== '/SignUp' && pathname !== '/Register' &&
@@ -67,7 +76,7 @@ export default function BottomNavigation({ rol }) {
 
                 }
                 <div className='relativebg-red-500 w-full flex justify-end'>
-                    <button className='absolute top-12 mt-12 right-5 z-50 bg-[#F7BE38] p-2 px-5 rounded-[5px] border' onClick={()=>router.push('/Admin')}>Administrador</button>
+                    <button className='absolute top-12 mt-12 right-5 z-50 bg-[#F7BE38] p-2 px-5 rounded-[5px] border' onClick={() => router.push('/Admin')}>Administrador</button>
                 </div>
                 <div className="hidden w-screen md:block md:w-auto " id="navbar-default">
                     <ul className="list-none font-medium flex flex-col p-4 md:p-0 mt-0 rounded-lg md:flex-row md:space-x-8  ">
